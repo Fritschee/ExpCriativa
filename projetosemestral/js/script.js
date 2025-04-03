f3435474752babc82d43acbc4dc2a1b32137a4eb
Seu Nome <seuemail@example.com>
2025-04-02 22:29:37 -0300

    Alterações senha, registro, etc

--- projetosemestral/js/script.js
+++ projetosemestral/js/script.js
@@ -9,3 +9,11 @@ function gotoIndex(){
 function gotoCart(){
     window.location.href = "../pages/cart.html"
 }
+
+function gotoProfile() {
+    window.location.href = "../pages/perfil.html";
+}
+
+function gotoAlterarSenha() {
+    window.location.href = "../pages/alterar-senha.html";
+}
