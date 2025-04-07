ee5b93bc706cb5aeeb53aaf5bd827ef605cc6670
Kauan Cornelsen <115079606+k1cornelsen@users.noreply.github.com>
2025-04-06 23:15:37 -0300

    Remoção XSS e bug visual do CARRINHO

--- projetosemestral/php/clear-carrinho.php
+++ projetosemestral/php/clear-carrinho.php
@@ -1,7 +1,13 @@
 <?php
-
+header('Content-Type: application/json');
 include "conexao.php";
 
 $sql = "DELETE FROM carrinho";
 
-mysqli_query($con, $sql);
+if (mysqli_query($con, $sql)) {
+    echo json_encode(['success' => true]);
+} else {
+    echo json_encode(['success' => false, 'message' => 'Erro ao limpar carrinho.']);
+}
+
+mysqli_close($con);
